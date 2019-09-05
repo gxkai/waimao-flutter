4723343a1f7b78bfa18e938b5c010d826af1b506
guxukai <1378026744@qq.com>
2019-09-05 13:19:08 +0800

    第一次进入加载动画

--- lib/views/flow_statistics/flow_statistics.dart
+++ lib/views/flow_statistics/flow_statistics.dart
@@ -4,6 +4,7 @@ import 'package:intl/intl.dart';
 import 'package:waimao/charts/combo_chart/date_time_line_point.dart';
 import 'package:waimao/model/visit_by_day_info.dart';
 import 'package:waimao/utils/data_utils.dart';
+import 'package:waimao/utils/progress_dialog.dart';
 import 'package:waimao/views/flow_statistics/visitors_info_select.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:waimao/views/home_page.dart';
@@ -16,13 +17,14 @@ class FlowStatistics extends StatefulWidget {
 }
 
 class FlowStatisticsState extends State<FlowStatistics> {
+  bool _loading = false;
   List<VisitByDayInfo> rows = [];
   RefreshController _refreshController =
-      RefreshController(initialRefresh: true);
+      RefreshController(initialRefresh: false);
 
   void _onRefresh() async {
     try {
-      await loadData();
+      await _loadData();
       // if failed,use refreshFailed()
       _refreshController.refreshCompleted();
     } catch (e) {
@@ -52,99 +54,120 @@ class FlowStatisticsState extends State<FlowStatistics> {
               }),
         ],
       ),
-      body: SmartRefresher(
-        enablePullDown: true,
-        enablePullUp: false,
-        header: WaterDropHeader(),
-        controller: _refreshController,
-        onRefresh: _onRefresh,
-        child: ListView(
-          padding: EdgeInsets.all(20),
-          children: <Widget>[
-            Card(
-                color: Colors.white,
-                child: Container(
-                    padding: EdgeInsets.all(10),
-                    child: Column(
-                      children: <Widget>[
-                        Row(
-                          children: <Widget>[
-                            Text(
-                              "最近30天浏览/访客量",
-                              style: TextStyle(color: Colors.black),
-                            )
-                          ],
-                        ),
-                        Container(
-                          height: 300,
-                          padding: EdgeInsets.symmetric(vertical: 5.0),
-                          child: ListView(
+      body: Stack(children: <Widget>[
+        SmartRefresher(
+          enablePullDown: true,
+          enablePullUp: false,
+          header: WaterDropHeader(),
+          controller: _refreshController,
+          onRefresh: _onRefresh,
+          child: ListView(
+            padding: EdgeInsets.all(20),
+            children: <Widget>[
+              Card(
+                  color: Colors.white,
+                  child: Container(
+                      padding: EdgeInsets.all(10),
+                      child: Column(
+                        children: <Widget>[
+                          Row(
                             children: <Widget>[
-                              DataTable(
-                                columnSpacing: 20,
-                                columns: [
-                                  DataColumn(
-                                    label: Text('日期'),
-                                  ),
-                                  DataColumn(
-                                    label: Text('浏览量uv'),
-                                  ),
-                                  DataColumn(
-                                    label: Text('访客量pv'),
-                                  ),
-                                ],
-                                rows: rows.map((row) {
-                                  return DataRow(cells: [
-                                    DataCell(Text(new DateFormat('yyyy-MM-dd')
-                                        .format(row.key))),
-                                    DataCell(Text(row.pv.toString(),textAlign: TextAlign.end,)),
-                                    DataCell(Text(row.uv.toString())),
-                                  ]);
-                                }).toList(),
-                              ),
+                              Text(
+                                "最近30天浏览/访客量",
+                                style: TextStyle(color: Colors.black),
+                              )
                             ],
                           ),
-                        )
-                      ],
-                    ))),
-            SizedBox(
-              height: 10,
-            ),
-            Card(
-                color: Colors.white,
-                child: Container(
-                    padding: EdgeInsets.all(10),
-                    child: Column(
-                      children: <Widget>[
-                        Row(
-                          children: <Widget>[
-                            Text(
-                              "最近30天浏览/访客量折线图",
-                              style: TextStyle(color: Colors.black),
-                            )
-                          ],
-                        ),
-                        Container(
-                          height: 200,
-                          child:
-                          DateTimeComboLinePointChart.withVisitByDayData(
-                              rows),
-                        )
-                      ],
-                    ))),
-          ],
+                          Container(
+                            height: 300,
+                            padding: EdgeInsets.symmetric(vertical: 5.0),
+                            child: ListView(
+                              children: <Widget>[
+                                DataTable(
+                                  columnSpacing: 20,
+                                  columns: [
+                                    DataColumn(
+                                      label: Text('日期'),
+                                    ),
+                                    DataColumn(
+                                      label: Text('浏览量uv'),
+                                    ),
+                                    DataColumn(
+                                      label: Text('访客量pv'),
+                                    ),
+                                  ],
+                                  rows: rows.map((row) {
+                                    return DataRow(cells: [
+                                      DataCell(Text(new DateFormat('yyyy-MM-dd')
+                                          .format(row.key))),
+                                      DataCell(Text(row.pv.toString(),textAlign: TextAlign.end,)),
+                                      DataCell(Text(row.uv.toString())),
+                                    ]);
+                                  }).toList(),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ))),
+              SizedBox(
+                height: 10,
+              ),
+              Card(
+                  color: Colors.white,
+                  child: Container(
+                      padding: EdgeInsets.all(10),
+                      child: Column(
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              Text(
+                                "最近30天浏览/访客量折线图",
+                                style: TextStyle(color: Colors.black),
+                              )
+                            ],
+                          ),
+                          Container(
+                            height: 200,
+                            child:
+                            DateTimeComboLinePointChart.withVisitByDayData(
+                                rows),
+                          )
+                        ],
+                      ))),
+            ],
+          ),
         ),
-      ),
+        ProgressDialog(
+            isLoading: _loading,
+            message: '正在加载...',
+            alpha: 0.35,
+            child: Container()),
+      ],)
     );
   }
 
   @override
   void initState() {
     // TODO: implement initState
+    _initData();
     super.initState();
   }
 
-  loadData() async {
+  void _initData() async {
+    try {
+      setState(() {
+        _loading = true;
+      });
+      await _loadData();
+    } finally {
+      setState(() {
+        _loading = false;
+      });
+    }
+  }
+
+  _loadData() async {
     var formatter = new DateFormat('yyyy-MM-dd');
     var from = new DateTime.now().add(new Duration(days: -29));
     var to = new DateTime.now();
